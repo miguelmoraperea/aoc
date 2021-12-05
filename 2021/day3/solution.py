04a338ac40b4054322c1cc78a3fea3cde280a41a
Miguel Mora Perea <morapmiguel@gmail.com>
2021-12-04 21:39:08 -0800

    ADD - Day 3, Part 2

--- 2021/day3/solution.py
+++ 2021/day3/solution.py
@@ -1,16 +1,21 @@
+from copy import deepcopy
+
+
 class Submarine:
 
-    def get_power_consuption(self, data):
-        num_of_readings = len(data)
-        len_of_reading = len(data[0])
+    def __init__(self, data):
+        self._data = data
+        self._num_of_readings = len(self._data)
+        self._len_of_reading = len(self._data[0])
 
+    def get_power_consuption(self):
         gamma_rate_str = ""
 
-        for col in range(len_of_reading):
+        for col in range(self._len_of_reading):
             cnt_0 = 0
             cnt_1 = 0
-            for row in range(num_of_readings):
-                if data[row][col] == "0":
+            for row in range(self._num_of_readings):
+                if self._data[row][col] == "0":
                     cnt_0 += 1
                 else:
                     cnt_1 += 1
@@ -21,3 +26,38 @@ class Submarine:
                                    for i in gamma_rate_str)
         epsilon_rate = int(epsilon_rate_str, base=2)
         return gamma_rate * epsilon_rate
+
+    def get_life_support_rating(self):
+        oxygen_generator = self._filter_data(True)
+        co2_scrubber = self._filter_data(False)
+        return oxygen_generator * co2_scrubber
+
+    def _filter_data(self, on_1):
+        numbers = deepcopy(self._data)
+
+        for col in range(self._len_of_reading):
+
+            cnt_0 = 0
+            cnt_1 = 0
+            row = 0
+
+            # Find most popular bit 0/1 for bit in col
+            while row < len(numbers):
+                if numbers[row][col] == "0":
+                    cnt_0 += 1
+                else:
+                    cnt_1 += 1
+                row += 1
+
+            if on_1:
+                keep = "0" if cnt_0 <= cnt_1 else "1"
+            else:
+                keep = "0" if cnt_0 > cnt_1 else "1"
+
+            # Get only values that contain "keep" in the column number
+            numbers = [number for number in numbers if number[col] == keep]
+
+            if len(numbers) == 1:
+                break
+
+        return int(numbers[0], base=2)
--- 2021/day3/test.py
+++ 2021/day3/test.py
@@ -11,6 +11,7 @@ class TestPart1(unittest.TestCase):
             "11110",
             "10110",
             "10111",
+            "10101",
             "01111",
             "00111",
             "11100",
@@ -19,13 +20,39 @@ class TestPart1(unittest.TestCase):
             "00010",
             "01010",
         ]
-        s = Submarine()
-        self.assertEqual(198, s.get_power_consuption(data))
+        s = Submarine(data)
+        self.assertEqual(198, s.get_power_consuption())
 
     def test_large_input(self):
         data = read_input()
-        s = Submarine()
-        print(s.get_power_consuption(data))
+        s = Submarine(data)
+        print(s.get_power_consuption())
+
+
+class TestPart2(unittest.TestCase):
+
+    def test_example(self):
+        data = [
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",
+        ]
+        s = Submarine(data)
+        self.assertEqual(230, s.get_life_support_rating())
+
+    def test_large_input(self):
+        data = read_input()
+        s = Submarine(data)
+        print(s.get_life_support_rating())
 
 
 def read_input():
